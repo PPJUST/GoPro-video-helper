12eccd08c48c7dccc719b87880c16c26193a0e92
PPJUST <106465946+PPJUST@users.noreply.github.com>
2023-03-31 16:04:49 +0800

    完成全部代码，支持GoPro5、7的视频命名规则

--- GoPro video helper.py
+++ GoPro video helper.py
@@ -1,51 +1,51 @@
-# gopro
+import os
+import time
+import subprocess
+import shutil
 
-import os, time, subprocess
-
-
-
-
-'''
-
-改名为文件日期-视频组
-
-是否合并视频
-
-GOPRO7文件名规则
-
-GH010320
-
-01为顺序，0320为组
-
-实际视频顺序：GH0320_01
-
-GORPO5规则
-
-GOPROxxxx开头的是第一段，GP01xxxx的是后续，最后四位xxxx一样的为同一段视频，01为顺序编号，4G一个视频
-
-目前思路：1.给出路径，遍历文件，检查后缀和文件名 2.对文件编号 3. 是否合并视频>>>如果合并，则将视频组放到一个文件夹中，外部只留合并后的视频文件
-
-'''
-is_merge = False  # 是否合并，默认否
 
 def get_path():
-    '''获取文件夹路径'''
-    file_path = input('输入GoPro视频所在文件夹：')
+    """获取文件夹路径"""
+    file_path = input('★★★输入GoPro视频所在文件夹： ')
     if os.path.exists(file_path) and os.path.isdir(file_path):
         walk_path(file_path)
     else:
-        print('文件夹不存在')
+        print('——————文件夹不存在——————')
         get_path()
-def ask_merge():
-    ask = input('是否合并视频 Y/N')
-    global is_merge
-    if ask == 'Y':
-        is_merge = True
-    elif ask == 'N':
-        is_merge = False
+
+
+def ask_merge(video_group_copy):
+    """合并视频"""
+    ask = input('★★★是否合并视频（Y/N）： ')
+    if ask.upper() == 'Y':
+        key_total = len(video_group_copy)
+        key_number = 0
+        for key in video_group_copy:
+            key_number += 1
+            inputfiles_write = ""
+            for i in sorted(video_group_copy[key]):
+                inputfiles_write += "file '" + i + "'\n"
+            with open('inputfiles.txt', 'w') as iw:
+                iw.write(inputfiles_write)
+            output_file = key
+            command = f"ffmpeg -f concat -safe 0 -i inputfiles.txt -c copy {output_file}"
+            subprocess.run(command)
+            print(f'——————已完成第{key_number}组视频合并，共{key_total}组——————')
+            os.remove('inputfiles.txt')
+            # 移动文件到文件夹
+            os.mkdir(os.path.splitext(key)[0])
+            for i in sorted(video_group_copy[key]):
+                shutil.move(i, os.path.splitext(key)[0])
+        print('——————已完成全部视频合并，并将分段视频移至单独文件夹——————')
+    elif ask.upper() == 'N':
+        pass
+    else:
+        print('——————输入不正确——————')
+        ask_merge(video_group_copy)
+
 
 def walk_path(path):
-    '''遍历文件'''
+    """遍历文件"""
     files = os.listdir(path)
     full_files = []
     for i in files:
@@ -57,8 +57,9 @@ def walk_path(path):
             video_files.append(i)  # 提取出mp4后缀的文件
     rename_files(video_files)
 
+
 def rename_files(files):
-    '''修改文件名'''
+    """修改文件名"""
     '''
     GoPro7文件名规则：
     例如：GH010320.mp4，GH010321.mp4，GH020320.mp4
@@ -70,130 +71,77 @@ def rename_files(files):
     '''
     check_first_file = os.path.split(files[0])[1]  # 提取第一个文件名
     if check_first_file.find('GH') != -1:
-        rule_gopro7(files)
+        run_rename(files, 'gopro7')
     elif check_first_file.find('GOPRO') != -1 or check_first_file.find('GP') != -1:
-        rule_gopro5(files)
+        run_rename(files, 'gopro5')
     else:
         pass  # 占位，不清楚其他型号的命名规则
 
-def rule_gopro7(files):
-    '''GoPro7重命名规则'''
+
+def run_rename(files, gopro_mode):
+    """GoPro7重命名规则"""
     # 命名思路：1.文件名后四位放到集合中，得到所有视频组编号 2.创建字典，key为视频组编号，value为视频分卷路径
     '''
     GoPro7文件名规则：
     例如：GH010320.mp4，GH010321.mp4，GH020320.mp4
     GH 为固定前缀，01 为视频组的第1个视频，0320 为视频编号，视频编号相同的为同一组视频
     '''
-    video_number = set()
     video_group = {}  # 存放最终分类结果，key编号1024-value完整文件名
-
-    def get_filename(x):
-        return os.path.splitext(os.path.split(x)[1])[0]
-
     for file in files:
         if get_filename(file)[-4:] not in video_group:  # 如果字典中没有该项，则添加空列表项
             video_group[get_filename(file)[-4:]] = []
         video_group[get_filename(file)[-4:]].append(file)
     video_group_copy = {}  # 用于合并视频的复制字典
     for key in video_group:
-        new_name = ''
         new_name_path = os.path.split((sorted(video_group[key])[0]))[0]
         new_name_group = str(key)
         new_name_time = os.path.getmtime(sorted(video_group[key])[0])
         new_name_time = time.gmtime(new_name_time)  # 标准化时间
-        new_name_time = time.strftime("%Y-%m-%d %H-%M-%S", new_name_time)  # 格式化时间
+        new_name_time = time.strftime("%Y%m%d-%H.%M.%S", new_name_time)  # 格式化时间
+        new_name_prefix = 'GoPro'
         new_name_suffix = os.path.splitext((sorted(video_group[key])[0]))[1]
         for value in sorted(video_group[key]):
-            new_name_number = get_filename(value)[-6:-4]
-            new_name = new_name_time + '_' + new_name_group + '_' + new_name_number + new_name_suffix
+            if gopro_mode == "gopro7":
+                new_name_number = get_filename(value)[-6:-4]
+            elif gopro_mode == "gopro5":
+                if value.find('GOPRO') != -1:  # 相比gopro7规则代码不同的地方
+                    new_name_number = '01'
+                else:
+                    new_name_number = "{:02d}".format(int(get_filename(value)[-6:-4]) + 1)
+            new_name = new_name_prefix + '_' + new_name_time + '_' + new_name_group + '_' + new_name_number + new_name_suffix
             new_full_name = os.path.join(new_name_path, new_name)
-            print(f'原文件名{value}')
-            print(f'新文件名{new_full_name}')
             os.rename(value, new_full_name)
 
             new_key = os.path.join(new_name_path, new_name_time + '_' + new_name_group + new_name_suffix)
             if new_key not in video_group_copy:
                 video_group_copy[new_key] = []
             video_group_copy[new_key].append(new_full_name)
-    print(video_group_copy)
-    ask_merge()
-    if is_merge:
-        for key in video_group_copy:
-            input_files = "|".join(sorted(video_group_copy[key]))
-            print(input_files)
-            output_file = key
-            ffmepg_path = 'ffmpeg.exe'
-            command = f'{ffmepg_path} -i "concat:{input_files}" -c copy "{output_file}"'
-            run_merge = subprocess.run(command)
+    print('——————全部视频已改名——————')
+    ask_merge(video_group_copy)
 
 
-def rule_gopro5(files):
-    '''GoPro5重命名规则'''
-    '''
-    GoPro5文件名规则：
-    例如：GOPRO1024.mp4，GP011024.mp4，GP021024.mp4
-    GOPRO 开头的为视频组的第1个视频，1024 为视频编号，视频编号相同的为同一组视频，后续GP01为顺序编号
-    '''
-    video_number = set()
-    video_group = {}  # 存放最终分类结果，key编号1024-value完整文件名
-    def get_filename(x):
-        return os.path.splitext(os.path.split(x)[1])[0]
-
-    for file in files:
-        if get_filename(file)[-4:] not in video_group:  # 如果字典中没有该项，则添加空列表项
-            video_group[get_filename(file)[-4:]] = []
-        video_group[get_filename(file)[-4:]].append(file)
-    video_group_copy = {}  # 用于合并视频的复制字典
-    for key in video_group:
-        new_name = ''
-        new_name_path = os.path.split((sorted(video_group[key])[0]))[0]
-        new_name_group = str(key)
-        new_name_time = os.path.getmtime(sorted(video_group[key])[0])
-        new_name_time = time.gmtime(new_name_time)  # 标准化时间
-        new_name_time = time.strftime("%Y-%m-%d %H-%M-%S", new_name_time)  # 格式化时间
-        new_name_suffix = os.path.splitext((sorted(video_group[key])[0]))[1]
-        for value in sorted(video_group[key]):
-            if value.find('GOPRO') != -1:  # 相比gopro7规则代码不同的地方
-                new_name_number = 0
-            else:
-                new_name_number = get_filename(value)[-6:-4] + 1
-            new_name = new_name_time + '_' + new_name_group + '_' + new_name_number + new_name_suffix
-            new_full_name = os.path.join(new_name_path, new_name)
-            print(f'原文件名{value}')
-            print(f'新文件名{new_full_name}')
-            os.rename(value, new_full_name)
-
-            new_key = os.path.join(new_name_path, new_name_time + '_' + new_name_group + new_name_suffix)
-            if new_key not in video_group_copy:
-                video_group_copy[new_key] = []
-            video_group_copy[new_key].append(new_full_name)
-    print(video_group_copy)
-    ask_merge()
-    if is_merge:
-        for key in video_group_copy:
-            input_files = "|".join(sorted(video_group_copy[key]))
-            print(input_files)
-            output_file = key
-            ffmepg_path = 'ffmpeg.exe'
-            command = f'{ffmepg_path} -i "concat:{input_files}" -c copy "{output_file}"'
-            run_merge = subprocess.run(command)
+def get_filename(x):
+    return os.path.splitext(os.path.split(x)[1])[0]
 
 
+def hello():
+    word = '''
+使用步骤：
+1. 输入GoPro视频所在文件夹路径，程序会自动改名。
+改名格式为“GoPro_拍摄日期-时间_内部编号_第几个视频”，示例GoPro_20230101-12.50.05_0021_01.mp4
+
+2. 改名完成后可以选择合并视频，合并方法为调用ffmepg无损合并。
+合并完成后，分段视频将会保存到单独的文件夹中。
+如果程序内没有ffmepg.exe请自行下载。
+
+        '''
+    print(word)
 
 
-def rule_gopromax(files):
-    '''GoPro Max重命名规则'''
-    pass
+def main():
+    hello()
+    get_path()
 
 
-
-get_path()
-
-'''
-GOPRO7文件名规则
-
-
-
-目前思路：1.给出路径，遍历文件，检查后缀和文件名 2.对文件编号 3. 是否合并视频>>>如果合并，则将视频组放到一个文件夹中，外部只留合并后的视频文件
-'''
-
+if __name__ == "__main__":
+    main()
